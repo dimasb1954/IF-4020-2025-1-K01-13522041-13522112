9c2dda3891b82d33d34a3d75c6845baf69550ab8
dimasb1954 <dimasbagoes1954@gmail.com>
2025-09-22 23:15:59 +0700

    feat: setup tailwind & typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-center p-6">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Hello Tailwind + React + TSS
+      </h1>
+    </div>
+  );
+}
+
+export default App;
